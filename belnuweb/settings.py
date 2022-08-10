6b3f373e577d650f42acfcf31e8b38e30efa2062
Pablo Mamonegit config --global user.email pmamone1@gmail.comgit commit -m primer <pmamone1@gmail.com>
2022-08-10 11:18:14 -0300

    ds

--- belnuweb/settings.py
+++ belnuweb/settings.py
@@ -13,12 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-PASSWORD_GMAIL = env('PASSWORD_GMAIL')
+PASSWORD_GMAIL = PASSWORD_GMAIL
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost','127.0.0.1','belnuweb.herokuapp.com']
 
