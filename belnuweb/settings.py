c2994aed278b54acd77dd42d0d9042b02cbf4600
Pablo Mamonegit config --global user.email pmamone1@gmail.comgit commit -m primer <pmamone1@gmail.com>
2022-08-11 21:00:16 -0300

    w

--- belnuweb/settings.py
+++ belnuweb/settings.py
@@ -170,9 +170,9 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'belnuweb.storage.WhiteNoiseStaticFilesStorage'
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
-#STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     'belnuweb/static',
 ]
